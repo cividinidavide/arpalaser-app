6e8851c4a96155fe6e3b540b357d39cd71610a59
Davide Cividini <davide.cividini@sorint.it>
2016-09-23 10:42:02 +0200

    Initial commit

---
+++ Arpa Laser/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Arpa_Laser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Liberare le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.ovalShape1 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Porta seriale:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(17, 53);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(142, 27);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(211, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connect to Arduino!";
+            this.button1.UseCompatibleTextRendering = true;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(17, 97);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBox1.Size = new System.Drawing.Size(370, 175);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(208, 275);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Suono:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(17, 353);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(142, 27);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(284, 339);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 41);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "STOP";
+            this.button2.UseCompatibleTextRendering = true;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.ovalShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(407, 392);
+            this.shapeContainer1.TabIndex = 8;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // ovalShape1
+            // 
+            this.ovalShape1.BackColor = System.Drawing.Color.Red;
+            this.ovalShape1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.ovalShape1.BorderColor = System.Drawing.Color.DimGray;
+            this.ovalShape1.Location = new System.Drawing.Point(213, 302);
+            this.ovalShape1.Name = "ovalShape1";
+            this.ovalShape1.Size = new System.Drawing.Size(15, 15);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(236, 300);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "DISCONNESSO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 392);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shapeContainer1);
+            this.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Arpa Laser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Arpa Laser/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using WMPLib; // Includo la libreria di Windows Media Player per la riproduzione di suoni.
+
+namespace Arpa_Laser
+{
+    public partial class Form1 : Form
+    {
+        // Dichiaro l'oggetto SerialPort.
+        SerialPort sp;
+
+        // Oggetto DataTime, lo utilizzo per sapere l'ora in cui scrivo nella TextBox.
+        static DateTime dt = DateTime.Now;
+        String time = dt.ToShortTimeString();
+        
+        // Variabile per contenere le porte dispobili.
+        string[] ports;
+
+        // Lettore musicale, serve per riprodurre i suoni.
+        WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            // Setto le proprietà dei miei oggetti. 
+            label2.Text = "";
+            label2.Enabled = false;
+            button2.Enabled = false;
+            comboBox2.Enabled = false;
+
+            // Permette di avere accesso alla form anche da diversi thread.
+            CheckForIllegalCrossThreadCalls = false;
+
+            // RichTextBox non editabile.
+            richTextBox1.ReadOnly = true;
+            richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+        }
+
+
+
+        // Tutte quelle opzioni che vengono caricate all'avvio del programma.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ports = SerialPort.GetPortNames();
+
+            // Aggiungo tutte le porte seriali disponibili alla comboBox.
+            if (ports.Length != 0)
+            {
+                foreach (string port in ports)
+                {
+                    comboBox1.Items.Add(port);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Non ci sono porte seriali disponibili!", "Errore...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                comboBox1.Enabled = false;
+                button1.Enabled = false;
+            }
+
+
+            //////////////
+            // Carico gli elementi nella comboBox2 -> Inserisco i tipi di suoni.
+            comboBox2.Items.Insert(0, "Arpa");
+            comboBox2.Items.Insert(1, "Batteria");
+            comboBox2.Items.Insert(2, "Chitarra");
+            comboBox2.Items.Insert(3, "Pianoforte");
+            // Imposto suono di default.
+            comboBox2.SelectedItem = "Arpa";
+        }
+
+
+
+        // Dichiaro una stringa per contenere la variabile porta, e in caso di porta selezionata,
+        // chiamo la funzione "connect" per connettermi alla relativa porta.
+        string com;
+        // ---> Al clic del bottone "Connect to Arduino".
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text.ToString() == "")
+            {
+                MessageBox.Show("Selezionare prima la porta seriale!", "Errore...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                button1.Enabled = false;
+                button2.Enabled = true;
+                com = comboBox1.Text.ToString();
+                connect(com);
+            }
+        }
+
+
+
+        // Con questa funzione assegno le proprietà all'oggetto stanziato prima -> SerialPort.
+        // Chiamo il Thread "myport_DataReceived" per ricevere i messaggi.
+        // Infine, apro la porta seriale per l'ascolto.
+        void connect(string nome_porta)
+        {
+            sp = new SerialPort(nome_porta, 9600, Parity.None, 8, StopBits.One);
+            // DTR -> Indica che l'oggetto sulla porta seriale è pronto sia a ricevere sia a scrivere.
+            sp.DtrEnable = true;
+            sp.DataReceived += new SerialDataReceivedEventHandler(myport_DataReceived);
+
+            // Provo ad aprire la porta...
+            try
+            {
+                // Apro la porta.
+                sp.Open();
+
+                richTextBox1.AppendText("[" + time + "] " + "CONNESSO!\n");
+                comboBox1.Enabled = false;
+                comboBox2.Enabled = true;
+                label2.Enabled = true;
+                label2.Text = "Connesso alla porta " + nome_porta;
+                label4.Text = "CONNESSO";
+                ovalShape1.BackColor = Color.Green;
+            }
+            // ... in caso negativo restituisco errore.
+            catch
+            {
+                MessageBox.Show("Impossibile stabilire una connessione con quella porta!", "Errore...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        // Con readTo() leggo ogni linea che mi viene mandata dall'Arduino e la salvo in una stringa. LEGGO FINO AL CARATTERE DI FINE RIGA.
+        // Copio la stringa nella TextBox per leggere il messaggio.
+        void myport_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string msg = sp.ReadTo("\r\n");
+            richTextBox1.AppendText("[" + time + "] " + msg + "\r\n");
+
+            // A questo punto chiamo la funzione "sound", con la quale gestisco i suoni.
+            sound(msg);
+        }
+
+
+
+        // Passo come parametro il valore della stringa in modo da gestirlo in questa funzione.
+        // In base al suono selezionato e alla nota inviata dall'Arduino, genero un suono diverso.
+        void sound(string sound)
+        {
+            // Se il suono selezionato è quello dell'arpa...
+            if (String.Equals(comboBox2.SelectedItem, "Arpa"))
+            {
+                if (String.Equals(sound, "Nota A"))
+                {
+                    wplayer.URL = "Sounds/Harp/A.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota B"))
+                {
+                    wplayer.URL = "Sounds/Harp/B.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota C"))
+                {
+                    wplayer.URL = "Sounds/Harp/C.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota D"))
+                {
+                    wplayer.URL = "Sounds/Harp/D.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota E"))
+                {
+                    wplayer.URL = "Sounds/Harp/E.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota F"))
+                {
+                    wplayer.URL = "Sounds/Harp/F.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota G"))
+                {
+                    wplayer.URL = "Sounds/Harp/G.mp3";
+                    wplayer.controls.play();
+                }
+            }
+
+            // Se il suono selezionato è quello della batteria...
+            if (String.Equals(comboBox2.SelectedItem, "Batteria"))
+            {
+                if (String.Equals(sound, "Nota A"))
+                {
+                    wplayer.URL = "Sounds/Drums/A.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota B"))
+                {
+                    wplayer.URL = "Sounds/Drums/B.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota C"))
+                {
+                    wplayer.URL = "Sounds/Drums/C.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota D"))
+                {
+                    wplayer.URL = "Sounds/Drums/D.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota E"))
+                {
+                    wplayer.URL = "Sounds/Drums/E.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota F"))
+                {
+                    wplayer.URL = "Sounds/Drums/F.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota G"))
+                {
+                    wplayer.URL = "Sounds/Drums/G.mp3";
+                    wplayer.controls.play();
+                }
+            }
+
+            // Se il suono selezionato è quello della chitarra...
+            if (String.Equals(comboBox2.SelectedItem, "Chitarra"))
+            {
+                if (String.Equals(sound, "Nota A"))
+                {
+                    wplayer.URL = "Sounds/Guitar/A.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota B"))
+                {
+                    wplayer.URL = "Sounds/Guitar/B.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota C"))
+                {
+                    wplayer.URL = "Sounds/Guitar/C.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota D"))
+                {
+                    wplayer.URL = "Sounds/Guitar/D.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota E"))
+                {
+                    wplayer.URL = "Sounds/Guitar/E.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota F"))
+                {
+                    wplayer.URL = "Sounds/Guitar/F.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota G"))
+                {
+                    wplayer.URL = "Sounds/Guitar/G.mp3";
+                    wplayer.controls.play();
+                }
+            }
+
+            // Se il suono selezionato è quello di un pianoforte...
+            if (String.Equals(comboBox2.SelectedItem, "Pianoforte"))
+            {
+                if (String.Equals(sound, "Nota A"))
+                {
+                    wplayer.URL = "Sounds/Piano/A.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota B"))
+                {
+                    wplayer.URL = "Sounds/Piano/B.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota C"))
+                {
+                    wplayer.URL = "Sounds/Piano/C.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota D"))
+                {
+                    wplayer.URL = "Sounds/Piano/D.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota E"))
+                {
+                    wplayer.URL = "Sounds/Piano/E.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota F"))
+                {
+                    wplayer.URL = "Sounds/Piano/F.mp3";
+                    wplayer.controls.play();
+                }
+                if (String.Equals(sound, "Nota G"))
+                {
+                    wplayer.URL = "Sounds/Piano/G.mp3";
+                    wplayer.controls.play();
+                }
+            }
+        }
+        
+
+        // Chiudo la porta aperta in precedenza e termino di conseguenza la connessione con la seriale.
+        // Inoltre attivo/disattivo gli oggetti in modo da ricreare la situazione iniziale in apertura di programma.
+        // ---> Al clic del bottone "STOP".
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comboBox1.Enabled = true;
+            comboBox2.Enabled = false;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            label2.Text = "";
+            label4.Text = "DISCONNESSO";
+            ovalShape1.BackColor = Color.Red;
+
+            sp.Close();
+
+            richTextBox1.AppendText("[" + time + "] " + "DISCONNESSO!\n");
+        }
+
+
+
+        // ComboBox che contiene le porte seriali disponili.
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Tutte le impostazioni sono gia' state definite precedentemente.
+        }
+
+
+
+        // RichTextBox che contiene tutti i comandi e messaggi scambiati.
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
